4a46940a4c33ff7ac1ab8011fb3a7611db156fff
Crystal <59953866+CrystalHEngineer@users.noreply.github.com>
2021-04-12 21:52:11 -0500

    Add files via upload
    First Java assignment and Pythaogorean assignment.

---
+++ 01-java-fundamentals/FirstJavaAssign.java
@@ -0,0 +1,14 @@
+public class FirstJavaAssign {
+    public static void main(String[] args) {
+        System.out.println("My name is Crystal.");
+        System.out.println("I am 25 years old.");
+        System.out.println("My hometown is Reno, NV.");
+    }
+
+    
+    public class Pythagorean {
+        public double calculateHypotenuse(int legA, int legB) {
+            return Math.sqrt(Math.pow(legA, 2) + Math.pow(legB, 2));
+        }
+    }
+}
---
+++ 01-java-fundamentals/PythagoreanTest.java
@@ -0,0 +1,12 @@
+
+
+public class PythagoreanTest {
+    public static void main(String[] args, Object Pythagorean) {
+        System.out.println("Calculating hypotenuse for triangle with sides 3, 4:");
+        System.out.println(Pythagorean.calculateHypotenuse(3, 4));
+        System.out.println("Calculating hypotenuse for triangle with sides 4, 5:");
+        System.out.println(Pythagorean.calculateHypotenuse(4, 5));
+        System.out.println("Calculating hypotenuse for triangle with sides 9, 10:");
+        System.out.println(Pythagorean.calculateHypotenuse(9, 10));
+    }
+}
