3b750156d1c6521703a57fda3b8acf358f439a90
Crystal Harper <crystalhaylen@gmail.com>
2021-04-24 03:07:07 -0500

    First springboot

---
+++ 04-java-spring/01-spring-fundamentals/01-strings/StringAssign/src/main/java/com/crystal/StringAssign/StringAssignApplication.java
@@ -0,0 +1,13 @@
+package com.crystal.StringAssign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringAssignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringAssignApplication.class, args);
+	}
+
+}
---
+++ 04-java-spring/01-spring-fundamentals/01-strings/StringAssign/src/test/java/com/crystal/StringAssign/StringAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crystal.StringAssign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringAssignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
